4dda42c9571e2ed2c772421e24c5b2a21a626e87
wzoroya <115761558+wzoroya@users.noreply.github.com>
2022-12-02 17:36:24 -0800

    Update main.py

--- Game/main.py
+++ Game/main.py
@@ -2,15 +2,71 @@ import pygame, sys
 import button
 from pygame.locals import *
 from pygame import mixer
-import data
+from data import encounters
+from battle import battle_sequence
 from map import Map
+from event1 import event_sequence
+import button2
+
+bottom_panel = 150
+screen_width = 800 
+screen_height = 400 + bottom_panel
 
 pygame.init()
 FINAL_ENCOUNTER = 11
-SCREEN_SIZE = (800, 400)
-screen = pygame.display.set_mode(SCREEN_SIZE)
-pygame.display.set_caption("Main Menu")
+
+screen = pygame.display.set_mode((screen_width, screen_height - bottom_panel))
 clock = pygame.time.Clock()
+
+quit_image = pygame.image.load("images/quit.png").convert_alpha()
+
+#Map Logic + animation
+def map():
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    campfire_img =  pygame.image.load('battle/img/icons/campfire.png').convert_alpha()
+    campfire_button = button2.Button(screen, 420, screen_height - bottom_panel + 70, campfire_img, 64, 64)
+    map = Map(0, FINAL_ENCOUNTER, screen)
+    run = True
+    while run:
+        screen.fill('black')
+        map.run()
+        panel_img = pygame.image.load('battle/img/icons/panel.png').convert_alpha()
+        screen.blit(panel_img,(0,screen_height - bottom_panel))
+        mx, my = pygame.mouse.get_pos()
+        current_button = map.nodes.sprites()[map.current_encounter].rect
+
+        if current_button.collidepoint ((mx, my)):
+            if click:
+                if map.current_encounter in {0, 1, 3, 4, 6, 7, 9, 10}:
+                    if battle_sequence() == False:
+                        run = False
+                        screen = pygame.display.set_mode((screen_width, screen_height - bottom_panel))
+                        map.current_encounter = 0
+                if map.current_encounter in {2, 5, 8}:
+                    event_sequence(encounters[map.current_encounter]["string_list"])
+                map.current_encounter += 1 
+        click = False
+        if campfire_button.draw():
+            pass
+        if quit_button.draw(screen):
+                run = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        clock.tick(60)
+        
+#Main menu logic+animation
+pygame.display.set_caption("Main Menu")
 
 #Background image
 background_image = pygame.image.load("images/background.png").convert_alpha()
@@ -23,19 +79,6 @@ def draw_background():
 def draw_text(text, font, color, x, y):
     title = font.render(text, True, color)
     screen.blit(title, (x, y))
-
-def map():
-    map = Map(0, FINAL_ENCOUNTER, screen)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        screen.fill('black')
-        map.run()
-
-        pygame.display.update()
-        clock.tick(60)
 
 #Font
 font_1 = pygame.font.SysFont("cambria", 65)
@@ -52,15 +95,26 @@ info_image = pygame.image.load("images/info.png").convert_alpha()
 audio_image = pygame.image.load('images/audio.png').convert_alpha()
 back1_image = pygame.image.load('images/back1.png').convert_alpha()
 back2_image = pygame.image.load('images/back2.png').convert_alpha()
+m1_image = pygame.image.load('images/m1.png').convert_alpha()
+m2_image = pygame.image.load('images/m2.png').convert_alpha()
+m3_image = pygame.image.load('images/m3.png').convert_alpha()
+m4_image = pygame.image.load('images/m4.png').convert_alpha()
+m5_image = pygame.image.load('images/m5.png').convert_alpha()
 
 #Button instances
 start_button = button.Button(start_image, 340, 70)
 settings_button = button.Button(settings_image, 340, 165)
 quit_button = button.Button(quit_image, 340, 260)
 info_button = button.Button(info_image, 380, 60)
-audio_button = button.Button(audio_image, 380, 160)
+audio_button = button.Button(audio_image, 210, 150)
 back1_button = button.Button(back1_image, 345, 260)
 back2_button = button.Button(back2_image, 345, 260)
+m1_button = button.Button(m1_image, 280, 150)
+m2_button = button.Button(m2_image, 350, 150)
+m3_button = button.Button(m3_image, 420, 150)
+m4_button = button.Button(m4_image, 490, 150)
+m5_button = button.Button(m5_image, 560, 150)
+
 
 mixer.init()
 mixer.music.load('music/bg_music.mp3')
@@ -89,18 +143,19 @@ while run:
 
         #check settings
         if menu_state == "settings":
-            if info_button.draw(screen):
-                menu_state = "info"
-            if audio_button.draw(screen):
-                menu_state = "audio"
+            audio_button.draw(screen)
+            if m1_button.draw(screen):
+                mixer.music.set_volume(0.2)
+            if m2_button.draw(screen):
+                mixer.music.set_volume(0.4)
+            if m3_button.draw(screen):
+                mixer.music.set_volume(0.6)
+            if m4_button.draw(screen):
+                mixer.music.set_volume(0.8)
+            if m5_button.draw(screen):
+                mixer.music.set_volume(1.0)
             if back1_button.draw(screen):
                 menu_state = "main"
-        
-        if menu_state == "info":
-            draw_text("Press RETURN To Fullscreen", font_2, white, 260, 100)
-            draw_text("Press ESC To Window", font_2, white, 280, 180)
-            if back1_button.draw(screen):
-                menu_state = "settings"
 
         if menu_state == "audio":
             if back1_button.draw(screen):
@@ -124,18 +179,17 @@ while run:
 
             #press SPACE to fullscreen
             elif event.key == pygame.K_RETURN:
-                screen = pygame.display.set_mode(SCREEN_SIZE, pygame.FULLSCREEN|HWSURFACE)
+                screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN|HWSURFACE)
                 pygame.display.flip()
                 
             #press ESC to window
             elif event.key == pygame.K_ESCAPE:
-                screen = pygame.display.set_mode(SCREEN_SIZE)
+                screen = pygame.display.set_mode((screen_width, screen_height))
 
     #update display
     pygame.display.update()
 
-#quit game
-pygame.quit()
+
 
 
 
